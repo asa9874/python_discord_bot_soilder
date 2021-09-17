1d95bea38f53bb564a247e5884cfa5fc46f0dbf7
asa9874 <84450816+asa9874@users.noreply.github.com>
2021-09-17 21:17:00 +0900

    Update main.py

--- main.py
+++ main.py
@@ -108,38 +108,24 @@ async def covid(ctx):
 async def 날씨(ctx,text):
 
 
-    url=(f'https://rp5.ru/'+text+'의_날씨')
-    driver=webdriver.Chrome()
+    url=(f'https://www.google.com/search?q='+text+'날씨')
+    driver=webdriver.Chrome(executable_path= r"/app/.chromedriver/bin/chromedriver")
     driver.get(url)
 
     soup = BeautifulSoup(driver.page_source, 'html.parser')
-    
-    Weather_tem=soup.select_one('#ArchTemp > span.t_0').text
+
+    Weather_img=soup.select_one('#wob_tci')['src']
+    Weather_rain=soup.select_one('#wob_pp').text
+    Weather_water=soup.select_one('#wob_hm').text
 
 
 
     embed = discord.Embed(title = text+"의 날씨",
     description = "", color = 0x62c1cc)
-    embed.add_field(name =  "온도:"+Weather_tem, value ="츄츄")
+    embed.set_thumbnail(url="http:" + Weather_img)
+    embed.add_field(name =  "비올확률:"+Weather_rain, value ="습도:"+Weather_water)
     await ctx.send(embed = embed)
 
-
-
-    embed = discord.Embed(title = text+"의 날씨",
-    description = "", color = 0x62c1cc)
-    embed.add_field(name =  "온도:"+Weather_tem, value =츄츄)
-    await ctx.send(embed = embed)
-
-
-
-
-
-
-    embed = discord.Embed(title = text+"의 날씨",
-    description = "", color = 0x62c1cc)
-    embed.set_thumbnail(url="http:" +Weather_img)
-    embed.add_field(name =  "온도:"+Weather_tem, value = "비올확률:"+Weather_rain+"습도:"+Weather_water)
-    await ctx.send(embed = embed)
 
 
 
