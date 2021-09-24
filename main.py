39bd296477f899cff3e7e7ee30463920596bb132
asa9874 <84450816+asa9874@users.noreply.github.com>
2021-09-25 02:31:18 +0900

    Update main.py

--- main.py
+++ main.py
@@ -23,8 +23,9 @@ import time
 
 # 옵션 생성
 options = webdriver.ChromeOptions()
-# 창 숨기는 옵션 추가
-options.add_argument("headless")
+렉줄이기
+prefs = {'profile.default_content_setting_values': {'cookies' : 2, 'images': 2, 'plugins' : 2, 'popups': 2, 'geolocation': 2, 'notifications' : 2, 'auto_select_certificate': 2, 'fullscreen' : 2, 'mouselock' : 2, 'mixed_script': 2, 'media_stream' : 2, 'media_stream_mic' : 2, 'media_stream_camera': 2, 'protocol_handlers' : 2, 'ppapi_broker' : 2, 'automatic_downloads': 2, 'midi_sysex' : 2, 'push_messaging' : 2, 'ssl_cert_decisions': 2, 'metro_switch_to_desktop' : 2, 'protected_media_identifier': 2, 'app_banner': 2, 'site_engagement' : 2, 'durable_storage' : 2}}   
+options.add_experimental_option('prefs', prefs)
 
 #executable_path= r"/app/.chromedriver/bin/chromedriver", options=options     크롬웹드라이버 클라우드할때 넣기
 
