334e99ee48d92c05c4ccff9fc7b16c670a7f0054
asa9874 <84450816+asa9874@users.noreply.github.com>
2022-06-19 02:16:34 +0900

    Update main.py

--- main.py
+++ main.py
@@ -28,7 +28,20 @@ options.add_experimental_option('prefs', prefs)
 game= discord.Game("군인의 삶")
 bot= commands.Bot(command_prefix='!',status=discord.Status.online,activity=game)
 
+from head import *
+from datetime import datetime
 
+
+@bot.command(aliases=['군대'])
+async def goon(ctx):
+    now  = datetime.now()
+    await ctx.send(f'현재'+str(now))
+
+    deadtime = datetime.strptime("20220920", "%Y%m%d")
+    await ctx.send(f'비교할 날짜'+str(deadtime))
+
+    date_diff = now - deadtime
+    await ctx.send(f'남은날'+str(deadtime-now))
 
 
 
