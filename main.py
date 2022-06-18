c6c0c211517e127b4f1533ee378e3c160c020792
asa9874 <84450816+asa9874@users.noreply.github.com>
2022-06-18 18:34:56 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,46 @@
-﻿from head import *
-from sadstory import *
+import discord
+from discord.ext import commands
+import random
+import requests
+from bs4 import BeautifulSoup
+import os
+
+import selenium
+from selenium import webdriver
+from selenium.webdriver import ActionChains
+
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import Select
+from selenium.webdriver.support.ui import WebDriverWait
+
+import time
+
+# 옵션 생성
+options = webdriver.ChromeOptions()
+# 랙제거
+prefs = {'profile.default_content_setting_values': {'cookies' : 2, 'images': 2, 'plugins' : 2, 'popups': 2, 'geolocation': 2, 'notifications' : 2, 'auto_select_certificate': 2, 'fullscreen' : 2, 'mouselock' : 2, 'mixed_script': 2, 'media_stream' : 2, 'media_stream_mic' : 2, 'media_stream_camera': 2, 'protocol_handlers' : 2, 'ppapi_broker' : 2, 'automatic_downloads': 2, 'midi_sysex' : 2, 'push_messaging' : 2, 'ssl_cert_decisions': 2, 'metro_switch_to_desktop' : 2, 'protected_media_identifier': 2, 'app_banner': 2, 'site_engagement' : 2, 'durable_storage' : 2}}   
+options.add_experimental_option('prefs', prefs)
+executable_path= r"/app/.chromedriver/bin/chromedriver", options=options     크롬웹드라이버 클라우드할때 넣기
+
+game= discord.Game("군인의 삶")
+bot= commands.Bot(command_prefix='!',status=discord.Status.online,activity=game)
+
+from datetime import datetime
 
 
+@bot.command(aliases=['군대'])
+async def goon(ctx):
+    now  = datetime.now()
+    await ctx.send(f'현재'+str(now))
 
+    deadtime = datetime.strptime("20220920", "%Y%m%d")
+    await ctx.send(f'비교할 날짜'+str(deadtime))
+
+    date_diff = now - deadtime
+    await ctx.send(f'남은날'+str(deadtime-now))
 
 
 
@@ -475,5 +513,7 @@ async def on_message_delete(message):
 
 #    print(name)
 
-bot.run('ODg3NzQ4NTcxMzA5NDE2NDY5.GFeEg2.ZbKG2yZmnrC_6Bzx6QUpgRFQNeh_t7SpwDlqvw')
 
+
+
+bot.run('ODg3NzQ4NTcxMzA5NDE2NDY5.GFeEg2.ZbKG2yZmnrC_6Bzx6QUpgRFQNeh_t7SpwDlqvw')
