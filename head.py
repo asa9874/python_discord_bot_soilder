4ed5657a16b7567421aae892c75240f9ed988420
asa9874 <84450816+asa9874@users.noreply.github.com>
2022-06-18 18:23:31 +0900

    Update head.py

--- head.py
+++ head.py
@@ -23,7 +23,7 @@ options = webdriver.ChromeOptions()
 # 랙제거
 prefs = {'profile.default_content_setting_values': {'cookies' : 2, 'images': 2, 'plugins' : 2, 'popups': 2, 'geolocation': 2, 'notifications' : 2, 'auto_select_certificate': 2, 'fullscreen' : 2, 'mouselock' : 2, 'mixed_script': 2, 'media_stream' : 2, 'media_stream_mic' : 2, 'media_stream_camera': 2, 'protocol_handlers' : 2, 'ppapi_broker' : 2, 'automatic_downloads': 2, 'midi_sysex' : 2, 'push_messaging' : 2, 'ssl_cert_decisions': 2, 'metro_switch_to_desktop' : 2, 'protected_media_identifier': 2, 'app_banner': 2, 'site_engagement' : 2, 'durable_storage' : 2}}   
 options.add_experimental_option('prefs', prefs)
-#executable_path= r"/app/.chromedriver/bin/chromedriver", options=options     크롬웹드라이버 클라우드할때 넣기
+executable_path= r"/app/.chromedriver/bin/chromedriver", options=options     크롬웹드라이버 클라우드할때 넣기
 
 game= discord.Game("군인의 삶")
 bot= commands.Bot(command_prefix='!',status=discord.Status.online,activity=game)
