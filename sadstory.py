34d06c65ae7f395f9996a8d33bd76c2a5e8b7f22
asa9874 <84450816+asa9874@users.noreply.github.com>
2022-06-19 20:47:43 +0900

    Update sadstory.py

--- sadstory.py
+++ sadstory.py
@@ -1,28 +1,42 @@
 from head import *
-from datetime import datetime
+from datetime import datetime, timedelta
+
+
+human={'곽호준':datetime.strptime("2022.01.24", "%Y.%m.%d").date(),
+       '지인혁':datetime.strptime("2022.01.24", "%Y.%m.%d").date(),
+       '윤지환':datetime.strptime("2022.01.24", "%Y.%m.%d").date(),
+       '정동호':datetime.strptime("2022.02.08", "%Y.%m.%d").date(),
+       '김영권':datetime.strptime("2022.03.15", "%Y.%m.%d").date(),
+       '김우현':datetime.strptime("2022.02.21", "%Y.%m.%d").date(),
+       '김윤석':datetime.strptime("2022.05.16", "%Y.%m.%d").date(),
+       '한동희':datetime.strptime("2022.05.23", "%Y.%m.%d").date(),
+       '임승현':datetime.strptime("2022.05.09", "%Y.%m.%d").date(),
+       '신승호':datetime.strptime("2022.06.14", "%Y.%m.%d").date(),
+       '박종범':datetime.strptime("2022.09.20", "%Y.%m.%d").date(),
+       '이병웅':datetime.strptime("2022.09.20", "%Y.%m.%d").date(),
+       '조재민':datetime.strptime("2022.09.20", "%Y.%m.%d").date(),
+       '천우혁':datetime.strptime("2022.07.25", "%Y.%m.%d").date(),
+       }
+
 
 
 @bot.command(aliases=['goon'])
 async def 군대(ctx,*,text):
-    now  = datetime.now().date()
-    if text=="곽호준":
-        endtime = datetime.strptime("20230723", "%Y%m%d").date()
-        
-    if text=="박종범" or text=="이병웅" or text=="조재민":
-        deadtime = datetime.strptime("20220920", "%Y%m%d").date()
-        endtime = datetime.strptime("20240319", "%Y%m%d").date()
-        lasttime= deadtime-now
-    
-    if(text=="박종범" or text=="이병웅" or text=="조재민" or text=="곽호준"):
+    if(text in human):
+        now  = datetime.now().date() #오늘 
+        endtime=human[text]+timedelta(days=546)
+        lasttime= endtime-now
         happytime=endtime-now
         embed = discord.Embed(title = text+"의 군대",
         description = "", color = 3066993)
         embed.set_thumbnail(url='https://upload.wikimedia.org/wikipedia/commons/thumb/e/eb/Military_Manpower_Administration_Gutgeoni1.png/200px-Military_Manpower_Administration_Gutgeoni1.png')
-        if text=="박종범" or text=="이병웅" or text=="조재민":
-            embed.add_field(name ='입대일',value =str(deadtime.year)+"년"+str(deadtime.month)+"월"+str(deadtime.day)+"일",inline=False)
-            embed.add_field(name ='남은입대일',value =str(lasttime))
+       
+        
+        if(lasttime> timedelta(days=546)):
+            deadtime=lasttime-timedelta(days=546)
+            embed.add_field(name ='남은입대일',value =str(deadtime))
         embed.add_field(name ='전역일',value =str(endtime.year)+"년"+str(endtime.month)+"월"+str(endtime.day)+"일",inline=False)
         embed.add_field(name ='남은전역일',value =str(happytime))
         await ctx.send(embed = embed) 
     else:
-        await ctx.send("몰?루")
+        await ctx.send("몰루")
