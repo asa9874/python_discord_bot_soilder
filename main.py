8a9f57cabccf43cfd0d616c2a9a63cebde41b70a
asa9874 <84450816+asa9874@users.noreply.github.com>
2022-06-19 13:51:00 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,23 +1,5 @@
 import discord
 from discord.ext import commands
-import random
-import requests
-from bs4 import BeautifulSoup
-import os
-
-import selenium
-from selenium import webdriver
-from selenium.webdriver import ActionChains
-
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.common.by import By
-
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.support.ui import Select
-from selenium.webdriver.support.ui import WebDriverWait
-
-import time
-
 
 game= discord.Game("버전 1.78v 페코라봇")
 bot= commands.Bot(command_prefix='!',status=discord.Status.online,activity=game)
