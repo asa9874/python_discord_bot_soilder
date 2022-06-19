e89f425e0471b4ea9b86edbe8dcf86718fbeeed3
asa9874 <84450816+asa9874@users.noreply.github.com>
2022-06-19 14:26:23 +0900

    Update sadstory.py

--- sadstory.py
+++ sadstory.py
@@ -1,15 +1,18 @@
-
 from head import *
 from datetime import datetime
 
 
 @bot.command(aliases=['군대'])
 async def goon(ctx):
-    now  = datetime.now()
-    await ctx.send(f'현재'+str(now))
-
-    deadtime = datetime.strptime("20220920", "%Y%m%d")
-    await ctx.send(f'비교할 날짜'+str(deadtime))
-
-    date_diff = now - deadtime
-    await ctx.send(f'남은날'+str(deadtime-now))
+    now  = datetime.now().date()
+    
+    deadtime = datetime.strptime("20220920", "%Y%m%d").date()
+    lasttime= deadtime-now
+    
+    embed = discord.Embed(title = "군대",
+    description = "", color = 3066993)
+    embed.set_thumbnail(url='https://upload.wikimedia.org/wikipedia/commons/thumb/e/eb/Military_Manpower_Administration_Gutgeoni1.png/200px-Military_Manpower_Administration_Gutgeoni1.png')
+    embed.add_field(name ='현재',value =str(now.year)+"년"+str(now.month)+"월"+str(now.day)+"일",inline=False)
+    embed.add_field(name ='입대일',value =str(deadtime.year)+"년"+str(deadtime.month)+"월"+str(deadtime.day)+"일",inline=False)
+    embed.add_field(name ='남은날',value =str(lasttime))
+    await ctx.send(embed = embed) 
