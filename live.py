546bc819fef0bb966eb44f7f11669c9b9dd7cf69
asa9874 <84450816+asa9874@users.noreply.github.com>
2022-06-19 21:56:14 +0900

    Update live.py

--- live.py
+++ live.py
@@ -29,7 +29,7 @@ async def 날씨(ctx,*,text):
     embed = discord.Embed(title = text+"의 날씨",
     description = "", color = 0x62c1cc)
     embed.set_thumbnail(url="http:" + Weather_img)
-    embed.add_field(name =  "비올확률:"+Weather_rain, value ="습도:")
+    embed.add_field(name =  "비올확률:"+Weather_rain, value ="습도:"+Weather_water)
     await ctx.send(embed = embed)
     driver.quit()
 
